d03f5cbc890a7f3322638eb1e9cce36caadf535d
whittle <brandonwwhittle@gmail.com>
2022-01-05 07:40:56 -0600

    CHORE(commands.ts): Renames `help.ts` to `commands.ts`

---
+++ src/commands/commands.ts
@@ -0,0 +1,72 @@
+import { ICommand } from 'wokcommands'
+
+export default {
+  category: 'Utility',
+  description: 'Get a list of tone indicator meanings',
+  slash: true,
+  testOnly: true,
+  syntax: '[command]',
+  options: [
+    {
+      name: 'commands',
+      description: 'The command you want more information about',
+      type: 3,
+      required: false,
+    },
+  ],
+
+  callback: ({ interaction, instance }) => {
+    const command = interaction.options.getString('command')
+    if (command) {
+      const match = instance.commandHandler.commands.filter(
+        (cmd) => cmd.names[0] === command
+      )[0]
+      if (!match) {
+        const failureEmbed = {
+          color: 0xff0000,
+          description: `Command "${command}" not found`,
+        }
+        return interaction.reply({ embeds: [failureEmbed], ephemeral: true })
+      }
+
+      const commandEmbed = {
+        color: 0xff9ed7,
+        title: match.names[0],
+        description: `Description: ${match.description}\nSyntax: ${match.syntax}`,
+      }
+      return interaction.reply({ embeds: [commandEmbed] })
+    }
+
+    const utilCommands: string[] = []
+    const modCommands: string[] = []
+
+    instance.commandHandler.commands.map((command) => {
+      if (command.category.includes('Utility')) {
+        utilCommands.push(command.names[0])
+      }
+      if (command.category.includes('Moderation')) {
+        modCommands.push(command.names[0])
+      }
+    })
+    const commandEmbed = {
+      color: 0xff9ed7,
+      title: 'Cirilla Commands',
+      fields: [
+        {
+          name: 'Utility',
+          value: '```YAML\n-' + utilCommands.join('\n-') + '\n```',
+          inline: true,
+        },
+        {
+          name: 'Moderation',
+          value: '```YAML\n-' + modCommands.join('\n-') + '\n```',
+          inline: true,
+        },
+      ],
+      footer: {
+        text: 'Use `/help <command>` for a description',
+      },
+    }
+    return interaction.reply({ embeds: [commandEmbed] })
+  },
+} as ICommand
