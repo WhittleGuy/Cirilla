182945e16224ba5c471a1a4f3a83df58e600b4b2
whittle <brandonwwhittle@gmail.com>
2022-01-07 05:47:43 -0600

    FIX(): Update `/clean` to require `MANAGE_MESSAGES`

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -4,8 +4,8 @@ import { FailureEmbed, SuccessEmbed } from '../helpers'
 export default {
   category: 'Moderation',
   description: 'Clean up the specified number of messages',
-  // permissions: ['ADMINISTRATOR'],
-  requireRoles: true,
+  permissions: ['MANAGE_MESSAGES'],
+  requireRoles: false,
   slash: true,
   testOnly: false,
   guildOnly: true,
