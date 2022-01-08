ba348f21f922afde17028c72fa8f5b2be826acee
whittle <brandonwwhittle@gmail.com>
2022-01-08 14:46:02 -0600

    FEAT(): Enable standard command usage for `/embed`

--- src/commands/embed.ts
+++ src/commands/embed.ts
@@ -8,7 +8,7 @@ export default {
   description: 'Post a custom embed',
   permissions: ['MANAGE_MESSAGES'],
   requireRoles: false,
-  slash: true,
+  slash: 'both',
   testOnly: false,
   guildOnly: true,
   options: [
@@ -38,33 +38,61 @@ export default {
     },
   ],
 
-  callback: async ({ interaction }) => {
-    const channel = interaction.options.getChannel('channel')
-    if (channel.type !== 'GUILD_TEXT')
-      return FailureEmbed(interaction, 'Please tag a text channel')
-    const title = interaction.options.getString('title')
-    const description = interaction.options.getString('description')
-    const color = interaction.options.getString('color') as ColorResolvable
-    const user = interaction.user
-
-    const postEmbed = await channel
-      .send({
-        embeds: [
-          {
-            color: ColorCheck(color),
-            title: title,
-            description: `${description ? description : ''}`,
-            footer: { text: user.id },
-            timestamp: new Date(),
-          },
-        ],
-      })
-      .catch((err) => {
-        return FailureEmbed(interaction, err)
-      })
-
-    if (postEmbed) {
-      return SuccessEmbed(interaction, `Embed posted in <#${channel.id}>`)
+  callback: async ({ guild, message, interaction }) => {
+    let channel, title, description, color
+    // Standard command
+    if (message) {
+      const idReg = /\d{18}/
+      let text = message.content.split(' ').slice(1)
+      const channelId = idReg.exec(text[0])[0] || null
+      if (channelId === null)
+        return await message.reply('Please tag a text channel')
+      channel = guild.channels.cache.get(channelId)
+      text.shift()
+      text = text.join(' ').split('|')
+      title = text[0]
+      description = text.slice(1)
     }
+    //Slash command
+    else {
+      channel = interaction.options.getChannel('channel')
+      title = interaction.options.getString('title')
+      description = interaction.options.getString('description')
+      color = interaction.options.getString('color') as ColorResolvable
+    }
+
+    if (channel.type !== 'GUILD_TEXT') {
+      if (message) return await message.reply('Please tag a text channel')
+      return FailureEmbed(interaction, 'Please tag a text channel')
+    }
+
+    const userId = message
+      ? guild.members.cache.get(message.author.id).user.id
+      : interaction.user.id
+
+    const postEmbed = await channel.send({
+      embeds: [
+        {
+          color: ColorCheck(color),
+          title: `${title ? title : ''}`,
+          description: `${description ? description : ''}`,
+          footer: { text: userId },
+          timestamp: new Date(),
+        },
+      ],
+    })
+
+    if (!postEmbed) {
+      if (message) return await message.reply('Error sending embed')
+      return FailureEmbed(interaction)
+    }
+
+    if (message) {
+      const reply = await message.reply('Embed sent')
+      await message.delete()
+      setTimeout(() => reply.delete(), 3000)
+      return
+    }
+    return SuccessEmbed(interaction, 'Embed sent')
   },
 } as ICommand
