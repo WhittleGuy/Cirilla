379750ba1bb0d1c223d77bf5d1d61f3ae536e41e
whittle <brandonwwhittle@gmail.com>
2022-01-10 05:40:39 -0600

    FIX(): Store data locally to avoid db calls

--- src/features/autodelete.ts
+++ src/features/autodelete.ts
@@ -1,17 +1,26 @@
-import { Client } from 'discord.js'
+import { Client, TextChannel } from 'discord.js'
 import autoDeleteSchema from '../models/auto-delete-schema'
+
+const autoDeleteData = {} as {
+  // guildId: [channel, message]
+  [key: string]: [string, number]
+}
 
 export default async (client: Client) => {
   client.on('messageCreate', async (message) => {
     const { guild, channel } = message
     if (channel.type !== 'GUILD_TEXT') return
 
-    const results = await autoDeleteSchema.findById(guild.id)
-    if (!results) return
+    let data = autoDeleteData[guild.id]
+    console.log(autoDeleteData)
 
-    const { channelId, timeout } = results
-    const data = [channelId, timeout]
-    console.log(data)
+    if (!data) {
+      const results = await autoDeleteSchema.findById(guild.id)
+      if (!results) return
+
+      const { channelId, timeout } = results
+      data = autoDeleteData[guild.id] = [channelId, timeout]
+    }
 
     if (channel.id !== data[0]) return
     else {
