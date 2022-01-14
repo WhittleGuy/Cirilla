9cab7d3db5837bf813c391e7a2c7eabf9738e3b2
whittle <brandonwwhittle@gmail.com>
2022-01-14 10:01:48 -0600

    REFACTOR(): Update colors

--- src/helpers/ColorCheck.ts
+++ src/helpers/ColorCheck.ts
@@ -1,15 +1,15 @@
 import { ColorResolvable } from 'discord.js'
 
 const ColorCheck = (color?: ColorResolvable | string): ColorResolvable => {
-  // const DEFAULT = '#ff9ed7'
-  const DEFAULT = '#F49CBB'
+  // const DEFAULT = '#FF9ED7'
+  const DEFAULT = '#FF9ED7'
   if (!color) return DEFAULT
-  else if (color === 'ADD') return '#23CE6B'
-  else if (color === 'REMOVE') return '#8F0000'
-  else if (color === 'STATUS') return '#6F58C9'
+  else if (color === 'ADD') return '#00B000'
+  else if (color === 'REMOVE') return '#B00000'
+  else if (color === 'STATUS') return '#FF9ED7'
   else if (color === 'PLAIN') return '#F4F1DE'
   else if (color === 'NONE') return '#2F3136'
-  else if (color === 'WARN') return '#D4660C'
+  else if (color === 'WARN') return '#FF9000'
   else if (color.toString().match(/^#[0-9a-f]{6}$/))
     return color as ColorResolvable
   else return DEFAULT
