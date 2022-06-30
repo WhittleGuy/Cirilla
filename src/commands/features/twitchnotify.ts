ae860d677c57df38224c4148f2143d03676f67c0
Brandon Whittle <brandonwwhittle@gmail.com>
2022-06-29 22:19:16 -0500

    FEAT(): Add live notifications for selected streamers

---
+++ src/commands/features/twitchnotify.ts
@@ -0,0 +1,78 @@
+import { ICommand } from 'wokcommands'
+import axios from 'axios'
+import { ColorCheck, SuccessMessage } from '../../helpers'
+import { arrayBuffer } from 'stream/consumers'
+
+const LIVE_STREAMERS = []
+const STREAMERS = ['asterixks', 'kloudmello', 'catssnap', 'vermillionaf']
+const HELIX = 'https://api.twitch.tv/helix'
+let authToken = ''
+
+export default {
+  category: 'Information',
+  description: 'Get the status of a streamer on Twitch',
+  // permissions: ['ADMINISTRATOR'],
+  requireRoles: false,
+  slash: false,
+  testOnly: false,
+  guildOnly: false,
+
+  init: async (client) => {
+    const res = await axios.post(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCHCLIENT}&client_secret=${process.env.TWITCHSECRET}&grant_type=client_credentials`
+    )
+    authToken = res.data['access_token']
+
+    const twitch = axios.create({
+      baseURL: HELIX,
+      timeout: 5000,
+      headers: {
+        'Client-ID': process.env.TWITCHCLIENT,
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+
+    const checkStatus = async () => {
+      console.log('called')
+      for (const i in STREAMERS) {
+        const {
+          data: { data: liveData },
+        } = await twitch.get(`/streams?user_login=${STREAMERS[i]}`)
+        if (liveData.length > 0) {
+          if (!LIVE_STREAMERS.includes(STREAMERS[i])) {
+            LIVE_STREAMERS.push(STREAMERS[i])
+            const {
+              data: { data: user },
+            } = await twitch.get(`/users?login=${STREAMERS[i]}`)
+            client.channels.fetch('930927322809393272').then((channel) =>
+              channel.send({
+                embeds: [
+                  {
+                    color: ColorCheck('STATUS'),
+                    thumbnail: {
+                      url: user[0].profile_image_url,
+                    },
+                    image: {
+                      url: `https://static-cdn.jtvnw.net/previews-ttv/live_user_${STREAMERS[i]}-960x540.jpg`,
+                    },
+                    description: `**[${liveData[0].title}](https://www.twitch.tv/${STREAMERS[i]})**
+                    \n**Game**\n${liveData[0].game_name}`,
+                  },
+                ],
+              })
+            )
+          }
+        } else {
+          const index = LIVE_STREAMERS.indexOf(STREAMERS[i])
+          if (index > -1) {
+            LIVE_STREAMERS.splice(index, 1)
+          }
+        }
+      }
+    }
+
+    setInterval(() => checkStatus(), 30000)
+  },
+
+  callback: async ({ interaction }) => {},
+} as ICommand
