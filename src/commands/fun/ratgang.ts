25eefd249adfbb6b897fba9a8527953d789301ce
whittle <brandonwwhittle@gmail.com>
2023-05-28 07:55:10 -0700

    FEAT(): Add RatGang command

---
+++ src/commands/fun/ratgang.ts
@@ -0,0 +1,36 @@
+import { ICommand } from 'wokcommands'
+import { FailureMessage, SuccessMessage } from '../../helpers'
+
+export default {
+  category: 'Fun',
+  description: 'Display the RatGang',
+  // permissions: ['ADMINISTRATOR'],
+  requireRoles: false,
+  slash: true,
+  testOnly: true,
+  guildOnly: false,
+  options: [],
+
+  callback: async ({ message, interaction }) => {
+    await interaction.deferReply()
+
+    const emoji = [
+      '<:RatGangCat:887203783258570762>',
+      '<:RatGangCcat:887203783963181087>',
+      '<:RatGangCheese:887203783237582888>',
+      '<:RatGangClaire:887203782633615391>',
+      '<:RatGangCreature:966071911958327389>',
+      '<:RatGangJulio:887203784260988948>',
+      '<:RatGangKloud:887203782423883807>',
+      '<:RatGangMouse:887203783380205638>',
+      '<:RatGangWhittle:887203782927200256>',
+      '<:RatGang_Dedos:887203780448370739>',
+    ]
+
+    try {
+      await interaction.editReply(`${emoji.join(' ')}`)
+    } catch {
+      await FailureMessage(interaction)
+    }
+  },
+} as ICommand
